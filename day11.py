39cb5c80b83239b1785b875a8968e377db88fa40
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2023-12-10 21:18:55 -0800

    day 11

---
+++ day11.py
@@ -0,0 +1,46 @@
+from util import *
+
+data = getlines("11")
+#data = getlines("11s")
+
+addrows = set([])
+for i,row in enumerate(data):
+    if '#' not in row:
+        addrows.add(i)
+
+addcols = set([])
+for j in range(len(data[0])):
+    if all(data[x][j] == '.' for x in range(len(data))):
+        addcols.add(j)
+
+galaxies = []
+for i,row in enumerate(data):
+    for j,val in enumerate(row):
+        if val == '#':
+            galaxies.append((i, j))
+
+def dist(i, j, addrows, mul):
+    if i > j:
+        return dist(j, i, addrows, mul)
+    res = 0
+    while i < j:
+        if i in addrows:
+            res += mul
+        else:
+            res += 1
+        i += 1
+    return res
+
+res = 0
+for a in range(len(galaxies)):
+    for b in range(a + 1, len(galaxies)):
+        res += dist(galaxies[a][0], galaxies[b][0], addrows, 2)
+        res += dist(galaxies[a][1], galaxies[b][1], addcols, 2)
+print(res)
+
+res = 0
+for a in range(len(galaxies)):
+    for b in range(a + 1, len(galaxies)):
+        res += dist(galaxies[a][0], galaxies[b][0], addrows, 1000000)
+        res += dist(galaxies[a][1], galaxies[b][1], addcols, 1000000)
+print(res)
---
+++ day11_part1only.py
@@ -0,0 +1,45 @@
+from util import *
+
+data = getlines("11")
+#data = getlines("11s")
+
+def addrow(grid, rowpos):
+    return grid[:rowpos] + [grid[rowpos]] + grid[rowpos:]
+
+def addcol(grid, colpos):
+    return [
+        row[:colpos] + row[colpos] + row[colpos:] for row in grid
+    ]
+
+addrows = []
+for i,row in enumerate(data):
+    if '#' not in row:
+        addrows.append(i)
+
+addrows.reverse()
+for r in addrows:
+    data = addrow(data, r)
+
+addcols = []
+for j in range(len(data[0])):
+    if all(data[x][j] == '.' for x in range(len(data))):
+        addcols.append(j)
+addcols.reverse()
+for c in addcols:
+    data = addcol(data, c)
+
+galaxies = []
+for i,row in enumerate(data):
+    for j,val in enumerate(row):
+        if val == '#':
+            galaxies.append((i, j))
+
+res = 0
+for a in range(len(galaxies)):
+    for b in range(a + 1, len(galaxies)):
+        res += abs(galaxies[a][0] - galaxies[b][0])
+        res += abs(galaxies[a][1] - galaxies[b][1])
+        print(galaxies[a], galaxies[b], abs(galaxies[a][0] - galaxies[b][0]), abs(galaxies[a][1] - galaxies[b][1]))
+print(galaxies)
+print(len(galaxies))
+print(res)
