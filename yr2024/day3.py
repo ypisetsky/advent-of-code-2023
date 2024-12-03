3f62c2d4ccab80a2a9155ee65b8f520d2293df4f
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2024-12-02 21:19:46 -0800

    both parts

--- yr2024/day3.py
+++ yr2024/day3.py
@@ -8,18 +8,18 @@ data = getlines(day)
 
 mul_regex = r'(?:(mul)\((\d+),(\d+)\))|(?:(do)\(\))|(?:(don\'t)\(\))'
 
-def get_score(line):
+def get_score(line, is_part2):
     all_matches = re.findall(mul_regex, line)
     res = 0
     on = True
     for match in all_matches:
-        print(match)
         if match[3] == "do":
             on = True
         elif match[4] == "don't":
             on = False
-        elif on:
+        elif on or not is_part2:
             res += int(match[1]) * int(match[2])
     return res
 
-print(get_score("".join(data)))
+print(get_score("".join(data), False))
+print(get_score("".join(data), True))
