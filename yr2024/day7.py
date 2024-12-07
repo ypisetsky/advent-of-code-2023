150acb089f5d8f25dcd9265fd001d5b83dd89726
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2024-12-06 21:39:43 -0800

    clarify

--- yr2024/day7.py
+++ yr2024/day7.py
@@ -7,20 +7,19 @@ res = 0
 
 def checkline(line, part):
     target = int(line[0][:-1])
-    previous = set([line[1]])
+    candidates = [line[1]]
 
     for current in line[2:]:
-        candidates = []
-        for val in previous:
-            candidates.append(val + current)
-            candidates.append(val * current)
+        new_candidates = []
+        for val in candidates:
+            if val > target:
+                continue
+            new_candidates.append(val + current)
+            new_candidates.append(val * current)
             if part == 2:
-                candidates.append(int(f"{val}{current}"))
-        previous = set()
-        for candidate in candidates:
-            if candidate <= target:
-                previous.add(candidate)
-    return target if target in previous else 0
+                new_candidates.append(int(f"{val}{current}"))
+        candidates = new_candidates
+    return target if target in candidates else 0
 
 
 print(sum([checkline(line, 1) for line in data]))
